3ae9b1fef3a37cad47a0544f047f0bd64312cd23
Umberto Pontes <oneberto@outlook.com>
2023-01-30 09:50:22 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const generateArray = (length: number) =>
+    [...(new Array(length).keys() as any)] as number[];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+            {generateArray(10).map((i) => (
+                <img
+                    className="flex"
+                    key={i}
+                    src={
+                        "https://images.unsplash.com/photo-1661961110218-35af7210f803?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
+                    }
+                    alt="Test"
+                />
+            ))}
+        </div>
+    );
+};
 
 export default App;
